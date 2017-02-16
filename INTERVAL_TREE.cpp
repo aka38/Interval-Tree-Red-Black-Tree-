4992e3680c6bc1e74fb4018ae5f802d0b3cb69a0
Aman Kumar Azad <amanazad38@gmail.com>
2017-02-16 19:04:56 +0530

    Update INTERVAL_TREE.cpp

--- INTERVAL_TREE.cpp
+++ INTERVAL_TREE.cpp
@@ -3,8 +3,6 @@ using namespace std;
 #define MP          make_pair
 #define PB          push_back
 #define LL          long long
-#define MAX         1000005
-#define mod         1000000007
 #define debug(a)    cout<<a<<"\n"
 #define sd(a)       scanf("%d",&a)
 #define sc(a)       scanf("%c",&a)
@@ -19,65 +17,330 @@ using namespace std;
 #define fast()      ios_base::sync_with_stdio(0)
 #define chloop(i,j) cout<<"i:"<<i<<" j:"<<j<<"\n"
 
-const double eps=1e-9;
-const double pi =acos(-1);
-struct node{
+
+struct node {
 int key;
 int low;
 int high;
 int color;
-node *left,*parent,*right;
+node* left;
+node* parent;
+node* right;
 
 };
+node* nil;
+node* root;
 
-
-void interval_insert(node *T, node* z)
+void R_right(node T,node* x)
 {
-  node* y=T->nil;
-  node* x=T->root;
-  while(x!=T->nil)
+    node* y = x->left ;
+    x->left = y->right;
+    y->right->parent = x;
+    y->parent = x-> parent;
+    if(x->parent == nil)
+        root=y;
+    else if(x == x->parent->left)
+        x->parent->left=y;
+    else
+        x->parent->right=y;
+    y->right=x;
+    x->parent=y;
+}
+void R_left(node T,node* x)
+{
+
+    node* y= x->right;
+    x->right=y->left;
+    y->left->parent=x;
+    y->parent=x->parent;
+    if(x->parent==nil) root=y;
+
+     else if(x==x->parent->left)
+        x->parent->left=y;
+    else
+        x->parent->right=y;
+    y->left=x;
+    x->parent=y;
+}
+void insert_fixup(node T,node* z)
+{
+    node* y;
+    while(z->parent->color==0)
+    {
+        if(z->parent==z->parent->parent->left)
+        {
+            y=z->parent->parent->right;
+            if(y->color==0)
+            {
+                z->parent->color=1;
+                y->color=1;
+                z->parent->parent->color=0;
+                z=z->parent->parent;
+            }
+            else
+            {
+                if(z==z->parent->right)
+                 {
+                     z=z->parent;
+                      R_left(T,z);
+                 }
+                z->parent->color=1;
+                z->parent->parent->color=0;
+                R_right(T,(z->parent->parent));
+            }
+        }
+            else
+            {
+                y=z->parent->parent->left;
+            if(y->color==0)
+            {
+                z->parent->color=1;
+                y->color=1;
+                z->parent->parent->color=0;
+                z=z->parent->parent;
+            }
+            else
+            {
+                if(z==z->parent->left)
+                 {
+                     z=z->parent;
+                      R_left(T,z);
+                 }
+                z->parent->color=1;
+                z->parent->parent->color=1;
+                R_right(T,z->parent->parent);
+            }
+            }
+
+        }
+     //debug(z->color<<z->parent<<z->parent);
+        root->color=1;
+    }
+
+
+
+
+void interval_insert(node T, node* z)
+{
+  node* y=nil;
+  node* x=root;
+  while(x!=nil)
   {
       y=x;
-      if(z->key < x->key)
+      if(z->low < x->low)
       {
           x=x->left;
       }
       else
         x=x->right;
   }
-    z->p=y;
-     if (y == T->nil)
-      T->root =z;
-     else if (z->key< y->key)
+    z->parent=y;
+     if (y == nil)
+      root =z;
+     else if (z->low< y->low)
         y->left=z;
      else y->right=z;
 
-    z->left =T->nil;
-    z->right=T->nil;
+    z->left =nil;
+    z->right=nil;
     z->color =0;
+    //debug(z->color<<z->parent<<z->parent);
     insert_fixup(T,z);
 
 }
-void interval_delete(node T, node* current)
+node* successor(node* z)
 {
-
+    node* y;
+    if(z->right!=nil)
+    {
+        while(z->left)
+            z=z->left;
+        return z;
+    }
+    y=z->parent;
+    while(y!=nil && z==y->right)
+    {
+        z=y;
+        y=y->parent;
+    }
+    return y;
 }
-node interval_search(node T, node* current)
+void delete_fix(node T,node* x)
 {
-
+    node* y;
+    node* w;
+    while(x!=root && x->color==1)
+    {
+        if(x==x->parent->left)
+        {
+            w=x->parent->right;
+            if(w->color==0)
+            {
+                w->color=1;
+                x->parent->color=0;
+                R_left(T,x->parent);
+                w=x->parent->right;
+            }
+            if(w->left->color==1 && w->right->color==1)
+            {
+                w->color=0;
+                x=x->parent;
+            }
+            else
+            {
+                if(w->right->color==1)
+                {
+                    w->left->color=1;
+                    w->color=0;
+                     R_right(T,w);
+                    w=x->parent->right;
+                }
+                w->color=x->parent->color;
+                x->parent->color=1;
+                w->right->color=1;
+                 R_left(T,x->parent);
+                x=root;
+            }
+        }
+        else
+        {
+            w=x->parent->left;
+            if(w->color==0)
+            {
+                 x->parent->color=0;
+                w->color=1;
+                R_right(T,x->parent);
+                w=x->parent->left;
+            }
+            if(w->right->color==1 && w->left->color==1)
+            {
+                w->color=0;
+                x=x->parent;
+            }
+        else
+        {
+            if(w->left->color==1)
+                {
+                    w->right->color=1;
+                    w->color=0;
+                     R_left(T,w);
+                    w=x->parent->left;
+                }
+                w->color=x->parent->color;
+                w->left->color=1;
+                x->parent->color=1;
+                R_right(T,x->parent);
+                x=root;
+        }
+    }
+ }
+    x->color=1;
 }
-vector < <int,int> > interval;
+void interval_delete(node T, node* z)
+{
+    node* y;
+    node* x;
+    if(z->left==nil || z->right==nil)
+        y=z;
+    else
+        y=successor(z);
+    debug(z->low);
+    debug(z->parent->low);
+    debug(y->parent->low);
+    if(y->left!=nil)
+        x=y->left;
+    else
+        x=y->right;
+  
+    debug(y->parent->low);
+    x->parent=y->parent;
+    if(y->parent==nil)
+       root=x;
+    else if(y==y->parent->left)
+        y->parent->left=x;
+    else
+        y->parent->right=x;
+    if(y!=z)
+    {
+        z->low=y->low;
+    }
+
+    if(y->color==1) delete_fix(T,x);
+}
+
+void dfs(node* x)
+{
+    if(x==nil) return;
+    dfs(x->left);
+    debug("LOW: "<<x->low<<" & LOW[parent] "<<x->parent->low);
+    dfs(x->right);
+}
+node* interval_search(node T,int i,int j)
+{
+    node* x=root;
+    while(x!=nil && (j<x->low || i>x->high))
+    {
+        if(x->left!=nil && x->left->key>=i) x=x->left;
+        else x=x->right;
+    }
+    //cout<<x->low<<" "<<x->high<<endl;
+    return x;
+}
+//void init(node *z)
+//{
+//    z=new struct node;
+// z->low=-1; z->high=-1; z->key=-1; z->color=1;
+// z->left=NULL; z->right=NULL;
+//}
+int interval[10][2];
+
 int main()
 {
- node *T=new node;
- T->low=-1; T->high=-1; T->key=-1; T->color=1;
- T->left=NULL; T->right=NULL;
-    interval.PB({15,20});
-    interval.PB({10,30});
-    interval.PB({17,19});
-    interval.PB({5,20});
-    interval.PB({12,15});
-    interval.PB({30,40});
+nil =new struct node;
+ node T;
+// init(nil);
+// init(root);
+
+ nil->low=-1; nil->high=-1; nil->key=-1; nil->color=1;
+ nil->left=NULL; nil->right=NULL,nil->parent=NULL;
+ T.low=-1; T.high=-1; T.key=-1; T.color=1;
+ T.left=nil; T.right=nil,T.parent=nil;
+ root =&T;
+ node *ptr;
+
+    interval[0][0]=15;
+    interval[0][1]=20;
+    interval[1][0]=10;
+    interval[1][1]=30;
+    interval[2][0]=17;
+    interval[2][1]=19;
+    interval[3][0]=5;
+    interval[3][1]=20;
+    interval[4][0]=12;
+    interval[4][1]=15;
+    interval[5][0]=30;
+    interval[5][1]=40;
+    for(int i=0;i<6;i++)
+    {
+        if(!i)
+        {
+          T.low=interval[i][0];
+          T.high=interval[i][1];
+          T.key=0;
+        }
+        else
+        {
+           node* temp=new struct node;
+            temp->left=nil;
+            temp->right=nil;
+            temp->parent=nil;
+            temp->color=0;
+            temp->low=interval[i][0];
+            temp->high=interval[i][0];
+            temp->key=0;
+            interval_insert(T,temp);
+        }
+    }
 
 
 }
